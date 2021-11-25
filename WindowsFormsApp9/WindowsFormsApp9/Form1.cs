ed175cb4fff344800464c7a67e9ec4a93e7df585
Maxim <78133224+argentoff1@users.noreply.github.com>
2021-11-25 13:44:27 +0300

    Project for UP

---
+++ WindowsFormsApp9/WindowsFormsApp9/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBox1.Text), b = double.Parse(textBox2.Text), step = double.Parse(textBox3.Text), x, y;
+                this.chart1.Series[0].Points.Clear();
+                x = a;
+                while (x <= b)
+                {
+                    y = Math.Tan(x) + Math.Sin(x);
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                    x += step;
+                }
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Ошибка! Введите число через запятую");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
